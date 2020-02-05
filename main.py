456bdbf220d4f37a19b718889b35089da899bf09
myassingment <laurentzgo@gmail.com>
2020-02-06 00:49:19 +0700

    Perbaiki beberapa, dan koreksi bagian Account

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ from os import system
 
 #Class
 
-def cls():
-    os.system("cls")
 
 class Account:
     class_list = {}
@@ -15,7 +13,6 @@ class Account:
     b = 2
     int
     c = 3
-    clear = lambda: os.system('cls')
     def __init__(self):
         print("=========================================")
         print("|       Welcome to Bank GankBank        |")
@@ -31,15 +28,20 @@ class Account:
         print("=========================================")
         print("|        Sign up an new account         |")
         print("=========================================")
-        self.Name = input("Input your name : ")
-        self.AccNum = input("Input Your Account Number : ")
-        self.Pin  = input("Input Your PIN : ")
+        self.__Name = input("Input your name : ")
+        self.__AccNum = input("Input Your Account Number : ")
+        self.__Pin  = input("Input Your PIN : ")
+        self.__ammount = float(input("Input your first deposite (min: $100) : "))
 
 
-        Account.class_list[Account.a] = self.AccNum
-        Account.class_list[Account.b] = self.Pin
+        Account.class_list[Account.a] = self.__AccNum  # Pake ini ga bisa simpan value lebih dari satu
+        Account.class_list[Account.b] = self.__Pin     # artinya gak bisa bikin 2 akun
+        Account.class_list[Account.c] = self.__ammount # gmna dong ? :(
         print("=========================================")
         print("Your Account Has Been Create")
+        print(Account.class_list[Account.a])
+        for i in Account.class_list[Account.a]:
+            print(i)
         
     def login(self):
         print("=========================================")
@@ -48,41 +50,58 @@ class Account:
 
         InAccNum = input("Input Your Account Number : ")
         InputPin = input("Input Your PIN :")
-
-        if InputPin == Account.class_list[Account.b] and InAccNum == Account.class_list[Account.a]:
-            print("Success")
-        else:
-            print("Failed")
+        for i in Account.class_list[Account.b]:
+            print(i)  #gw cuma test valuenya apa,
+            # if InputPin == Account.class_list[Account.b][i] and InAccNum == Account.class_list[Account.a][i]:
+            #     print("Success")
+            # else:
+            #     print("Failed")
 
     def summon(self):
         s = Account()
         s.menu()
         choice = int(input("Input Your Choice : "))
-        if choice is 1:
-            s.create_account()
-        elif choice is 2 :
-            s.login()
-        else:
-            print("Thank Your, Have A Nice Day :)")
+        while True:
+            if choice is 1:
+                s.create_account()
+                break
+            elif choice is 2 :
+                s.login()
+                break
+            else:
+                print("Thank Your, Have A Nice Day :)")
+
+
+
+class ATM(Account):
+    def __init__(self, withdraw):
+        self.withdraw = 0
+
+    def makeWithdraw(self):
+        withdraw = float(input("Input Ammount for withdraw: "))
+        Account.class_list[Account.c]-= withdraw
+        print("You withdraw: " + str(withdraw)+ "Success")
+
+
 
 
 s = Account()
-s.menu()
-choice = int(input("Input Your Choice : "))
-if choice is 1 :
-    s.create_account()
-    choice3 = str(input("Press Y to go to Menu : "))
-    if choice3 is 'y' or choice3 is 'Y':
-        s.summon()
 
-elif choice is 2 :
-    print("Please Create Your Account Before Sign Up")
-    choice2 = str(input("Press Y to Sign Up : "))
-    if choice2 is 'y' or choice2 is 'Y':
+while True:
+    s.menu()
+    choice = int(input("Input Your Choice : "))
+    if choice == 1 :
         s.create_account()
-        s.summon()
-elif choice is 3 :
-    print("Thank Your, Have A Nice Day :)")
+        choice3 = str(input("Press Y to go to Menu : "))
+        if choice3 == 'y' or choice3 == 'Y':
+            pass
+
+    elif choice == 2 :
+        s.login()
+        pass
+    elif choice == 3 :
+        print("Thank Your, Have A Nice Day :)")  
+        break
 
 
 
