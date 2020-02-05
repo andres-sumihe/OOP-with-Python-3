b45e697ab19da5d5b7b0e2a4e4614d04c5a29ec0
myassingment <laurentzgo@gmail.com>
2020-02-05 15:29:49 +0700

    Struktur file

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import math
 
 #Class
+class Account():
+    def __init__self(self):
+
+    
+class ATM(Account):
+    def __init__self(self):
+
+class ATM_2(Account):
+    def __init__self(self):
 
 
 #Inheritance
