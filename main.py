180f6068e04532bdeebfe2df4b9ae9fe5f960a87
SeanAles <SeanAles@users.noreply.github.com>
2020-02-05 02:55:32 -0800

    Update main.py
    just do some stuff

--- main.py
+++ main.py
@@ -1,19 +1,46 @@
 import math
+from os import system
 
 
 #Class
-class Account:
-    def __init__self(self):
-        self.nama = nama
-        print("Hello")
-        
-    def contoh(self):
-        nama = input("Masukan nama: ")
-        print("Nama anda : ",nama)
-s = Account()
-s.contoh()
+class Bank:
+    def __init__(self):
+        self.data = []
+        print("===============================")
+        print("Selamat Datang di Bank GankBank")
+        print("===============================")
+
+    def Create_Account(self):
+        print("\n===================")
+        print("Create Your Account")
+        print(  "===================")
+        self.name = input("Masukkan Nama Lengkap Anda : ")
+        self.pin  = input("Masukkin Pin Anda : ")
+        self.id   = input("Masukkin ID Anda :")
+        self.data.append(self.name)
+
+    def menu(self):
+        print("1. Create Account")
+        print("2. Delete Account")
+        print("3. Exit")
     
 
+s = Bank()
+s.menu()
+s.Create_Account()
+
+my_dict = {
+    self.pin : [self.nama, self.ammount, self.rekening]
+}
+
+
+my_dict = {}
+
+for i in my_dict:
+    if pin == my_dict[i] and nama == my_dict[i][0]:
+        print("login berhasil")
+        a = _system('cls')
+        atm()
 #Inheritance
 
 
